77e4788c3e7db9d252a50285f5d480292ddfb7a4
MEpping <42342124+MEpping@users.noreply.github.com>
2018-08-21 14:40:34 +0200

    win32 work-around
    Winsock redefinition issue
    no unistd.h
    _USE_MATH_DEFINES

--- include/displayobjectdialog.h
+++ include/displayobjectdialog.h
@@ -6,7 +6,9 @@
 ///
 
 #pragma once
-
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include "globject.h"
 
 #include <QDialog>
--- include/osireader.h
+++ include/osireader.h
@@ -15,7 +15,6 @@
 
 #include <fstream>
 #include <iostream>
-#include <unistd.h>
 
 #include <QObject>
 #include <QMutex>

79ba9bc5cd856ba41f36f982e7f62f279feb98cc
Pierre R. Mai <pmai@pmsf.de>
2019-11-06 11:31:24 +0100

    Split install script, adjust docs

---
+++ build-install-build-deps.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+echo "
+#################################
+# Installing OSI 3                                                                    
+#################################
+"
+git submodule update --init
+cd open-simulation-interface
+git clone https://github.com/OpenSimulationInterface/proto2cpp.git
+mkdir -p build
+cd build
+cmake ..
+make -j8
+make install
+cd ../..
+
+
+echo "
+#################################
+# Installing FMI 2.0.2                                                                  
+#################################
+"
+mkdir -p fmi_library
+cd fmi_library
+
+FMI_lib_version=2.0.2
+
+fmi_library_include_install_dir=/usr/local/include/fmi-library
+fmi_library_lib_install_dir=/usr/local/lib/fmi-library
+
+if ! [ -e ${fmi_library_include_install_dir} -a -e ${fmi_library_lib_install_dir} ]
+then
+echo "
+Downloading FMI library...
+"
+
+if [ ! -d $FMI_lib_version ]
+then
+  wget --no-parent -nH --cut-dirs=2 -r https://svn.jmodelica.org/FMILibrary/tags/$FMI_lib_version/
+fi
+
+echo "
+Building FMI library...
+"
+  cd ${FMI_lib_version}
+  mkdir -p build
+  cd build
+  cmake ../. -DCMAKE_BUILD_TYPE=RELEASE
+  make -j8 install
+  cd ..
+  if [[ -d "install" ]]
+  then
+    cd install
+      mkdir -p ${fmi_library_include_install_dir}
+    if [[ -d ${fmi_library_include_install_dir} ]]
+    then
+      cp -uvrf ./include/* ${fmi_library_include_install_dir}/
+    fi
+    mkdir -p ${fmi_library_lib_install_dir}
+    if [[ -d ${fmi_library_lib_install_dir} ]]
+    then
+      cp -uvrf ./lib/* ${fmi_library_lib_install_dir}/
+    fi
+  else
+    echo "Could not install the fmi-library into usr/local"
+  fi
+fi
+
+cd ..
---
+++ install-deps.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+echo "
+#################################
+# Installing git                                                                 
+#################################
+"
+apt-get install git -y
+
+echo "
+#################################
+# Installing cmake                                                                 
+#################################
+"
+apt-get install cmake autoconf automake libtool curl make g++ unzip libtool pkg-config build-essential -y
+
+echo "
+#################################
+# Installing Qt                                                                 
+#################################
+"
+apt-get install qt5-default -y
+
+echo "
+#################################
+# Installing ZMQ                                                                   
+#################################
+"
+apt-get install libzmq5 libzmq3-dev -y
+
+apt-get install doxygen doxygen-doc doxygen-gui graphviz libeigen3-dev libboost-dev ocl-icd-opencl-dev -y
+
+echo "
+#################################
+# Installing protobuf                                                                   
+#################################
+"
+apt-get install libprotobuf-dev protobuf-compiler -y
+
+echo "
+#################################
+# Installing OpenGL                                                                   
+#################################
+"
+apt-get install libgl1-mesa-dev libgl1-mesa-dri libgl1-mesa-glx -y
